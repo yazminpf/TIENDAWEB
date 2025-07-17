c5618b5340b04b047bd263161c7979d17f050293
yazmin.p <luyaz1995@gmail.com>
2025-07-16 23:01:53 -0500

    Primer commit del proyecto tiendaweb

--- tienda/settings.py
+++ tienda/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jmczs3&$)cdq4evbkw3jx@@sved@b#b5tk3p=ik1rhz$^au#e2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['latienda-5ir1.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ROMULOB\Documents\tiendaweb\venv
+set VIRTUAL_ENV=D:\Bootcamp\tiendaweb\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
